6614177989ca8d84b357b0fe516587ba7e9ea2a2
Nguyễn Phước Thành <93478665+Zeres-Engel@users.noreply.github.com>
2024-07-05 14:53:50 +0700

    nest2d

--- test_RL_learning.py
+++
@@ -1,4 +0,0 @@
-from src.RL_learning import RLLearning
-
-rl_learning = RLLearning()
-rl_learning.run()
--- test_random.py
+++
@@ -1,4 +0,0 @@
-from src.random_learning import RandomLearning
-
-random_learning = RandomLearning()
-random_learning.run()
