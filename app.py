10ddd831b1d29e2fe98d8e32693c6b778f85636c
Nguyễn Phước Thành <93478665+Zeres-Engel@users.noreply.github.com>
2024-08-14 12:56:58 +0700

    nesting button

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-from src import Nester, Material, Object
+from flask import Flask, render_template
 
-def main():
-    material = Material(width=1000, height=1000,)
+app = Flask(__name__, static_folder='app/static', template_folder='app/templates')
 
-    nester = Nester()
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-    nester.add_material(material)
-    
-    ob1 = Object(img_path='data/obj1.jpg')
-    
-    nester.add_object(ob1, num_copies=17)
-    nester.nest()
-    nester.export_map()
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
