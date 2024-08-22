697cf2cd7ebba963ba5ed47673495fc3d82c414d
Nguyễn Phước Thành <93478665+Zeres-Engel@users.noreply.github.com>
2024-08-22 11:23:18 +0700

    transform agian

--- utils/transform.py
+++ utils/transform.py
@@ -13,16 +13,16 @@ def sort_points(points, clockwise=True):
     
     return points[sorted_indices]
 # Utility functions
-def rotate_points(points, angle, origin=(0, 0)):
+def rotate_point(point, angle, origin=(0, 0)):
+    """Rotate a point around a given origin."""
     ox, oy = origin
-    px, py = points.T  # Transpose to get x, y columns
+    px, py = point
     cos_angle, sin_angle = np.cos(angle), np.sin(angle)
 
     qx = ox + cos_angle * (px - ox) - sin_angle * (py - oy)
     qy = oy + sin_angle * (px - ox) + cos_angle * (py - oy)
-
-    return np.column_stack([qx, qy])
-
+    
+    return qx, qy
 
 def calculate_centroid(points):
     """Calculate the centroid of a set of points."""
@@ -30,8 +30,9 @@ def calculate_centroid(points):
 
 def transform_svg_element(elem, polygon, dx=0, dy=0, angle=0):
     """Apply transformation to an SVG element based on polygon and translation."""
-    polygon_centroid = calculate_centroid(polygon) + np.array([dx, dy])
-    # Chuyển đổi chức năng dựa trên nhãn SVG tag
+    polygon_centroid = calculate_centroid(polygon)
+    polygon_centroid = polygon_centroid + np.array([dx, dy])
+    print(polygon_centroid)
     tag = elem.tag.split('}')[-1]
 
     transform_functions = {
